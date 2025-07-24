b48a010f10c92032f682aa86d1246b57e7c4680f
Md Sabbir Hossain <hossainsabbir309@gmail.com>
2025-07-24 17:53:11 +0600

    changes on portfolio item

--- src/components/portfolio/All.jsx
+++ src/components/portfolio/All.jsx
@@ -12,7 +12,7 @@ const All = () => {
   return (
     <>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 '>
-        <Card image={forever} title='E-commarce Website' liveLink='https://clothing-website-azure.vercel.app' codeLink='https://github.com/sabbirhossain-dev/clothing-website.git' className="h-full"/>
+        <Card image={forever} title='E-commerce Website' liveLink='https://clothing-website-azure.vercel.app' codeLink='https://github.com/sabbirhossain-dev/clothing-website.git' className="h-full"/>
       <Card image={bizzency} title='Business Website' liveLink='https://sabbirhossain-dev.github.io/Bizzency/' codeLink='https://github.com/sabbirhossain-dev/Bizzency.git'/>
         <Card image={hunt} title='Business Website' liveLink='https://sabbirhossain-dev.github.io/Hunt/' codeLink='https://github.com/sabbirhossain-dev/Hunt.git'/>
         <Card image={personalWebsite} title='Personal Website' liveLink='https://portfolio-website-pi-lac-81.vercel.app/' codeLink='https://github.com/sabbirhossain-dev/portfolio-website.git'/>
--- src/components/portfolio/Development.jsx
+++ src/components/portfolio/Development.jsx
@@ -11,7 +11,7 @@ const Development = () => {
   return (
     <>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 '>
-        <Card image={forever} title='E-commarce Website' liveLink='https://clothing-website-azure.vercel.app' codeLink='https://github.com/sabbirhossain-dev/clothing-website.git' className="h-full"/>
+        <Card image={forever} title='E-commerce Website' liveLink='https://clothing-website-azure.vercel.app' codeLink='https://github.com/sabbirhossain-dev/clothing-website.git' className="h-full"/>
         <Card image={hunt} title='Business Website' liveLink='https://sabbirhossain-dev.github.io/Hunt/' codeLink='https://github.com/sabbirhossain-dev/Hunt.git'/>
         <Card image={personalWebsite} title='Personal Website' liveLink='https://portfolio-website-pi-lac-81.vercel.app/' codeLink='https://github.com/sabbirhossain-dev/portfolio-website.git'/>
         <Card image={gym} title='Gymnasium Website' liveLink='https://sabbirhossain-dev.github.io/Gym-by-sabbir/' codeLink='https://github.com/sabbirhossain-dev/Gym-by-sabbir.git'/>
